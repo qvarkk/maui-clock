1c1716edacaec0a0a6c34397e1eaab96badfcf4e
Ilya <xumdes@gmail.com>
2024-10-19 19:41:38 +0500

    Remade whole thing :p

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,322 +4,110 @@ namespace clock
 {
     public partial class MainPage : ContentPage
     {
+        Dictionary<int, long> numbersMasks = new Dictionary<int, long>
+        {
+            { 0, 0b01110_10001_10001_10001_10001_10001_01110 },
+            { 1, 0b00100_01100_10100_00100_00100_00100_11111 },
+            { 2, 0b01110_10001_00001_00010_00100_01000_11111 },
+            { 3, 0b01110_10001_00001_00010_00001_10001_01110 }, 
+            { 4, 0b10001_10001_10001_11111_00001_00001_00001 },
+            { 5, 0b11111_10000_10000_01110_00001_00001_11110 },
+            { 6, 0b01110_10001_10000_11110_10001_10001_01110 },
+            { 7, 0b11111_00001_00001_00010_00100_01000_10000 },
+            { 8, 0b01110_10001_10001_01110_10001_10001_01110 },
+            { 9, 0b01110_10001_10001_01111_00001_10001_01110 }
+        };
+
+        Dictionary<Grids, Grid> numbersGrids;
+
+        public enum Grids { Hours_1 = 1, Hours_2, Minutes_1, Minutes_2, Seconds_1, Seconds_2 };
+
         TimeOnly time = TimeOnly.FromDateTime(DateTime.Now);
 
-        [Obsolete]
+        private PeriodicTimer _timer;
+        private CancellationTokenSource _cancellationTokenSource;
+
         public MainPage()
         {
             InitializeComponent();
 
-            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            numbersGrids = new Dictionary<Grids, Grid>
             {
-                Device.BeginInvokeOnMainThread(() => updateTime());
-                return true;
-            });
+                { Grids.Hours_1, HoursDisplay_1 },
+                { Grids.Hours_2, HoursDisplay_2 },
+                { Grids.Minutes_1, MinutesDisplay_1 },
+                { Grids.Minutes_2, MinutesDisplay_2 },
+                { Grids.Seconds_1, SecondsDisplay_1 },
+                { Grids.Seconds_2, SecondsDisplay_2 }
+            };
 
-            WriteTime();
+            StartTimer();
+        }
+
+        private async void StartTimer()
+        {
+            _timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                while (await _timer.WaitForNextTickAsync(_cancellationTokenSource.Token))
+                {
+                    updateTime();
+                }
+            } catch (OperationCanceledException)
+            {
+                Console.WriteLine("Timer canceled.");
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            StopTimer();
+        }
+
+        private void StopTimer()
+        {
+            _cancellationTokenSource?.Cancel();
+            _timer?.Dispose();
         }
 
         public void updateTime()
         {
             time = TimeOnly.FromDateTime(DateTime.Now);
-            Display.Clear();
             WriteTime();
         }
 
         public void WriteTime()
         {
-            WriteNumber(time.Hour / 10);
-            WriteNumber(time.Hour % 10);
-            WriteColon();
-            WriteNumber(time.Minute / 10);
-            WriteNumber(time.Minute % 10);
-            WriteColon();
-            WriteNumber(time.Second / 10);
-            WriteNumber(time.Second % 10);
+            WriteNumber(time.Hour / 10, Grids.Hours_1);
+            WriteNumber(time.Hour % 10, Grids.Hours_2);
+            WriteNumber(time.Minute / 10, Grids.Minutes_1);
+            WriteNumber(time.Minute % 10, Grids.Minutes_2);
+            WriteNumber(time.Second / 10, Grids.Seconds_1);
+            WriteNumber(time.Second % 10, Grids.Seconds_2);
         }
 
-        public void WriteNumber(int number)
+        public void WriteNumber(int number, Grids gridId)
         {
-            Grid grid = new Grid
-            {
-                HeightRequest = 140,
-                WidthRequest = 100,
-                RowSpacing = 5,
-                ColumnSpacing = 5,
-                RowDefinitions =
-                {
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition()
-                },
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition(),
-                    new ColumnDefinition(),
-                    new ColumnDefinition(),
-                    new ColumnDefinition(),
-                    new ColumnDefinition()
-                }
-            };
+            Grid grid = numbersGrids[gridId];
+            grid.Clear();
 
-            switch(number)
+            long mask = numbersMasks[number];
+            int i = 0;
+            for (int row = 0; row < grid.RowDefinitions.Count; row++)
             {
-                case 0:
-                    WriteZero(grid);
-                    break;
-                case 1:
-                    WriteOne(grid);
-                    break;
-                case 2:
-                    WriteTwo(grid);
-                    break;
-                case 3:
-                    WriteThree(grid);
-                    break;
-                case 4:
-                    WriteFour(grid);
-                    break;
-                case 5:
-                    WriteFive(grid);
-                    break;
-                case 6:
-                    WriteSix(grid);
-                    break;
-                case 7:
-                    WriteSeven(grid);
-                    break;
-                case 8:
-                    WriteEight(grid);
-                    break;
-                case 9:
-                    WriteNine(grid);
-                    break;
+                for (int col = 0; col < grid.ColumnDefinitions.Count; col++)
+                {
+                    int bitValue = (int)((mask >> 34 - i++) & 1);
+                    
+                    if (bitValue == 1)
+                    {
+                        grid.Add(CreateTile(), col, row);
+                    }
+                }
             }
-
-            Display.Add(grid);
-        }
-
-        public void WriteColon()        {
-            Grid grid = new Grid
-            {
-                HeightRequest = 140,
-                WidthRequest = 100,
-                RowSpacing = 5,
-                ColumnSpacing = 5,
-                RowDefinitions =
-                {
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition(),
-                    new RowDefinition()
-                },
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition()
-                }
-            };
-
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 1, 5);
-
-            Display.Add(grid);
-        }
-
-        public void WriteZero(Grid grid)
-        {
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 1, 4);
-            grid.Add(CreateTile(), 5, 4);
-            grid.Add(CreateTile(), 1, 5);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 1, 6);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-        }
-
-        public void WriteOne(Grid grid)
-        {
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 2, 2);
-            grid.Add(CreateTile(), 3, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 3, 3);
-            grid.Add(CreateTile(), 3, 4);
-            grid.Add(CreateTile(), 3, 5);
-            grid.Add(CreateTile(), 3, 6);
-            grid.Add(CreateTile(), 1, 7);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-            grid.Add(CreateTile(), 5, 7);
-        }
-
-        public void WriteTwo(Grid grid)
-        {
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 3, 5);
-            grid.Add(CreateTile(), 2, 6);
-            grid.Add(CreateTile(), 1, 7);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-            grid.Add(CreateTile(), 5, 7);
-        }
-
-        public void WriteThree(Grid grid)
-        {
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 1, 6);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-        }
-
-        public void WriteFour(Grid grid)
-        {
-            grid.Add(CreateTile(), 1, 1);
-            grid.Add(CreateTile(), 5, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 1, 4);
-            grid.Add(CreateTile(), 2, 4);
-            grid.Add(CreateTile(), 3, 4);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 5, 4);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 5, 7);
-        }
-
-        public void WriteFive(Grid grid)
-        {
-            grid.Add(CreateTile(), 1, 1);
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 5, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 2, 4);
-            grid.Add(CreateTile(), 3, 4);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 1, 6);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-        }
-
-        public void WriteSix(Grid grid)
-        {
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 1, 4);
-            grid.Add(CreateTile(), 2, 4);
-            grid.Add(CreateTile(), 3, 4);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 1, 5);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 1, 6);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-        }
-
-        public void WriteSeven(Grid grid)
-        {
-            grid.Add(CreateTile(), 1, 1);
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 5, 1);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 3, 5);
-            grid.Add(CreateTile(), 2, 6);
-            grid.Add(CreateTile(), 1, 7);
-        }
-
-        public void WriteEight(Grid grid)
-        {
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 2, 4);
-            grid.Add(CreateTile(), 3, 4);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 1, 5);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 1, 6);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
-
-        }
-
-        public void WriteNine(Grid grid)
-        {
-            grid.Add(CreateTile(), 2, 1);
-            grid.Add(CreateTile(), 3, 1);
-            grid.Add(CreateTile(), 4, 1);
-            grid.Add(CreateTile(), 1, 2);
-            grid.Add(CreateTile(), 5, 2);
-            grid.Add(CreateTile(), 1, 3);
-            grid.Add(CreateTile(), 5, 3);
-            grid.Add(CreateTile(), 2, 4);
-            grid.Add(CreateTile(), 3, 4);
-            grid.Add(CreateTile(), 4, 4);
-            grid.Add(CreateTile(), 5, 4);
-            grid.Add(CreateTile(), 5, 5);
-            grid.Add(CreateTile(), 1, 6);
-            grid.Add(CreateTile(), 5, 6);
-            grid.Add(CreateTile(), 2, 7);
-            grid.Add(CreateTile(), 3, 7);
-            grid.Add(CreateTile(), 4, 7);
         }
 
         public Rectangle CreateTile()
